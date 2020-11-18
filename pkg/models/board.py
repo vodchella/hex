1c9ee608c9178b5a2bab27b82c889a2cf98d834a
vodchella <twister.kz@gmail.com>
2020-11-18 16:26:58 +0600

    Refactor

--- pkg/models/board.py
+++ pkg/models/board.py
@@ -6,7 +6,7 @@ class Board:
     def __init__(self, width, height):
         self._width = width
         self._height = height
-        self._cells = [0 for _ in range(width * height)]
+        self._cells = [0] * width * height
 
     def _get_index_from_xy(self, x, y):
         return (y - 1) * self._width + (x - 1)

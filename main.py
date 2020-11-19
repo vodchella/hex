a8eff3770941f5d93d6ab67ea6f68c7fc941c460
vodchella <twister.kz@gmail.com>
2020-11-19 13:59:04 +0600

    Trim zero bytes from the end of result array

--- main.py
+++ main.py
@@ -4,16 +4,13 @@ from pkg.views.binary_board_view import BinaryBoardView
 from pkg.views.console_board_view import ConsoleBoardView
 
 if __name__ == '__main__':
-    board = Board(3, 3)
+    board = Board(4, 4)
     board.set_cell_xy(1, 1, PLAYER_TWO)
     board.set_cell_xy(2, 1, PLAYER_TWO)
     board.set_cell_xy(3, 1, PLAYER_TWO)
-    board.set_cell_xy(1, 2, PLAYER_TWO)
-    board.set_cell_xy(2, 2, PLAYER_NONE)
-    board.set_cell_xy(3, 2, PLAYER_ONE)
-    # board.set_cell_xy(1, 3, PLAYER_ONE)
-    # board.set_cell_xy(2, 3, PLAYER_TWO)
-    # board.set_cell_xy(3, 3, PLAYER_TWO)
+    board.set_cell_xy(1, 3, PLAYER_TWO)
+    board.set_cell_xy(2, 3, PLAYER_NONE)
+    board.set_cell_xy(3, 3, PLAYER_ONE)
 
     c_view = ConsoleBoardView(board)
     c_view.render()
--- pkg/views/binary_board_view.py
+++ pkg/views/binary_board_view.py
@@ -1,5 +1,8 @@
 from pkg.models.board import Board
 
+CELLS_IN_BYTE = 4
+BITS_PER_CELL = 2
+
 
 class BinaryBoardView:
     _board: Board = None
@@ -14,21 +17,25 @@ class BinaryBoardView:
             byte = 0
 
             for (i, cell) in enumerate(self._board.get_cells(), start=1):
-                cells_left = i % 4
+                cells_left = i % CELLS_IN_BYTE
                 byte_completed = cells_left == 0
 
                 byte += cell
                 if not byte_completed:
-                    byte = byte << 2
+                    byte = byte << BITS_PER_CELL
 
                 if byte_completed and i > 1:
-                    print(bin(byte))
                     result.append(byte)
                     byte = 0
 
                 if i == board_size and not byte_completed:
-                    byte = byte << (2 * (4 - cells_left - 1))
-                    print(bin(byte))
+                    byte = byte << (BITS_PER_CELL * (CELLS_IN_BYTE - cells_left - 1))
                     result.append(byte)
+
+            while len(result):
+                if result[-1]:
+                    break
+                else:
+                    result.pop()
 
             return result

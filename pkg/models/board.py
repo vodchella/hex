eddeb9165aed222b1b84d4d29453e5f5b71d8aa9
vodchella <twister.kz@gmail.com>
2020-11-20 17:46:19 +0600

    Add _check_bounds to Board

--- pkg/models/board.py
+++ pkg/models/board.py
@@ -5,17 +5,19 @@ BOARD_MAX_SIZE = 11
 
 
 class Board:
+    _check_bounds: bool = True
     _width: int = 0
     _height: int = 0
     _cells = []
 
-    def __init__(self, width, height, cells=None):
-        if not cells:
+    def __init__(self, width, height, check_bounds=True, cells=None):
+        if check_bounds:
             if width < 1 or height < 1:
                 raise BoardDimensionsException('Board size can\'t be less than one')
             if width > BOARD_MAX_SIZE or height > BOARD_MAX_SIZE:
                 raise BoardDimensionsException('Board size can\'t be greater than eleven')
 
+        self._check_bounds = check_bounds
         self._width = width
         self._height = height
         self._cells = cells if cells else [PLAYER_NONE] * width * height
@@ -25,7 +27,7 @@ class Board:
             raise BoardIndexOutOfBounds(f'Index {index} is out of board bounds')
 
     def _get_index_from_xy(self, x, y):
-        if x > BOARD_MAX_SIZE or y > BOARD_MAX_SIZE:
+        if self._check_bounds and (x > BOARD_MAX_SIZE or y > BOARD_MAX_SIZE):
             coord = x if x > y else y
             raise BoardCoordinateOutOfBounds(f'Coordinate {coord} is out of board bounds')
         return (y - 1) * self._width + (x - 1)
@@ -46,13 +48,15 @@ class Board:
         self._cells[self._get_index_from_xy(x, y)] = player
 
     def get_cell_by_index(self, index):
-        self._check_board_index(index)
+        if self._check_bounds:
+            self._check_board_index(index)
         return self._cells[index]
 
     def set_cell_by_index(self, index, player):
-        self._check_board_index(index)
+        if self._check_bounds:
+            self._check_board_index(index)
         self._cells[index] = player
 
     def copy(self):
         copied_cells = self._cells.copy()
-        return Board(self._width, self._height, copied_cells)
+        return Board(self._width, self._height, cells=copied_cells)

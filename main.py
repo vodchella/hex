b382fa0a36bddac7e3c1e8b43dde0806f4c01963
vodchella <twister.kz@gmail.com>
2020-11-26 15:19:54 +0600

    Search for destination node

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+from pkg.ai.pathfinders.astar import AStar
 from pkg.constants.game import PLAYER_ONE, PLAYER_TWO
 from pkg.models.board import Board
 from pkg.views.console_board_view import ConsoleBoardView
 
 if __name__ == '__main__':
     board = Board(3, 3)
-    board.set_cell(2, 0, PLAYER_ONE)
-    board.set_cell(1, 1, PLAYER_ONE)
-
-    neighbors = board.get_cell_neighbors(2, 0, exclude_players=[PLAYER_ONE])
-    for n in neighbors:
-        board.set_cell(n[0], n[1], PLAYER_TWO)
+    board.set_cell(0, 1, PLAYER_TWO)
+    board.set_cell(1, 1, PLAYER_TWO)
 
     c_view = ConsoleBoardView(board)
     c_view.render()
+
+    pf = AStar(board)
+    pf.find_path(PLAYER_ONE, 0, 0, 0, 2)
--- pkg/ai/pathfinders/astar.py
+++ pkg/ai/pathfinders/astar.py
@@ -1,4 +1,58 @@
+from pkg.constants.game import PLAYER_ONE, PLAYER_TWO
 from pkg.models.board import Board
+
+
+class Node:
+    _id: int = None
+    _x: int = None
+    _y: int = None
+    _cost = None
+    _previous = None
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+        self._id = y * 666 + x
+
+    def __eq__(self, other):
+        return self._id == other.get_id()
+
+    def __str__(self):
+        return f'({self._x}, {self._y})'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def get_id(self):
+        return self._id
+
+    def get_x(self):
+        return self._x
+
+    def set_x(self, x):
+        self._x = x
+
+    def get_y(self):
+        return self._y
+
+    def set_y(self, y):
+        self._y = y
+
+    def get_cost(self):
+        return self._cost
+
+    def set_cost(self, cost):
+        self._cost = cost
+
+    def get_previous(self):
+        return self._previous
+
+    def set_previous(self, previous):
+        self._previous = previous
+
+
+def choose_node(nodes):
+    return nodes[0]
 
 
 class AStar:
@@ -11,4 +65,23 @@ class AStar:
         self._board = board
 
     def find_path(self, for_player, src_x, src_y, dst_x, dst_y):
+        board = self._board
+        goal_node = Node(dst_x, dst_y)
+        opponent = PLAYER_ONE if for_player == PLAYER_TWO else PLAYER_TWO
+        reachable = [Node(src_x, src_y)]
+        explored = []
+        while len(reachable) > 0:
+            node = choose_node(reachable)
+            if node == goal_node:
+                return
+
+            reachable.remove(node)
+            explored.append(node)
+            new_reachable = [Node(x, y) for (x, y) in board.get_cell_neighbors(node.get_x(), node.get_y(), [opponent])]
+            new_reachable = [n for n in filter(lambda n: n not in explored, new_reachable)]
+
+            for adjacent in new_reachable:
+                if adjacent not in reachable:
+                    reachable.append(adjacent)
+
         pass

c80345c00cb4b2804439f2549f275f6e1a52cc7b
vodchella <twister.kz@gmail.com>
2020-12-02 19:17:54 +0600

    Created ChainPathfinder._find_chains()

--- pkg/ai/pathfinders/chain.py
+++ pkg/ai/pathfinders/chain.py
@@ -1,6 +1,6 @@
-from pkg.ai.pathfinders import Node, INFINITY
+from pkg.ai.pathfinders import Node, INFINITY, to_nodes
 from pkg.ai.pathfinders.basic import BasicPathfinder
-from pkg.constants.game import PLAYER_NONE
+from pkg.constants.game import PLAYER_NONE, PLAYER_ONE, PLAYER_TWO
 from pkg.models.board import Board
 from pkg.utils.hex import get_distance
 
@@ -30,7 +30,35 @@ class ChainPathfinder(BasicPathfinder):
     def __init__(self, board):
         super().__init__(board, choose_node)
 
+    def _find_chains(self, for_player):
+        result = []
+        opponent = PLAYER_ONE if for_player == PLAYER_TWO else PLAYER_TWO
+        explored = []
+        board = self._board
+        w, h = board.get_dimensions()
+        for y in range(h):
+            for x in range(w):
+                current_node = Node(x, y)
+                if current_node not in explored:
+                    player = board.get_cell(x, y)
+                    if player == for_player:
+                        chain = []
+                        reachable = [current_node]
+                        while len(reachable) > 0:
+                            node = reachable[0]
+                            reachable.remove(node)
+                            explored.append(node)
+                            chain.append(node)
+                            cells = board.get_cell_neighbors(node.x(), node.y(), [opponent, PLAYER_NONE])
+                            new_reachable = [n for n in filter(lambda n: n not in explored, to_nodes(cells, INFINITY))]
+                            for adjacent in new_reachable:
+                                if adjacent not in reachable:
+                                    reachable.append(adjacent)
+                        result.append(chain)
+        return result
+
     def find_path(self, for_player, src_x, src_y, dst_x, dst_y):
+        print(self._find_chains(for_player))
         board = self._board
         path = super().find_path(for_player, src_x, src_y, dst_x, dst_y)
         return [(x, y) for (x, y) in filter(lambda c: board.get_cell(c[0], c[1]) == PLAYER_NONE, path)]

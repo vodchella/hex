2773ad21a339a02e9db2dd5b235701cf8029d20e
vodchella <twister.kz@gmail.com>
2020-12-02 18:46:06 +0600

    Split sample board to chains

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from pkg.views.console_board_view import ConsoleBoardView
 
 if __name__ == '__main__':
     board = Board(7, 7)
+    board.set_cell(4, 0, PLAYER_ONE)
     board.set_cell(3, 0, PLAYER_ONE)
     board.set_cell(2, 1, PLAYER_ONE)
-    board.set_cell(1, 2, PLAYER_ONE)
+    # board.set_cell(1, 2, PLAYER_ONE)
     board.set_cell(1, 3, PLAYER_ONE)
     board.set_cell(1, 4, PLAYER_ONE)
     board.set_cell(2, 4, PLAYER_ONE)
-    board.set_cell(2, 5, PLAYER_ONE)
+    # board.set_cell(2, 5, PLAYER_ONE)
     board.set_cell(3, 5, PLAYER_ONE)
     board.set_cell(3, 6, PLAYER_ONE)
     board.set_cell(4, 6, PLAYER_ONE)
+    board.set_cell(5, 6, PLAYER_ONE)
 
     pf = ChainPathfinder(board)
     path = pf.find_path(PLAYER_ONE, 6, 0, 6, 6)

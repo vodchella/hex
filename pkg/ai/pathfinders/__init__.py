c48724ed0f26abf7c6e1c668e88023b02bf6fc8c
vodchella <twister.kz@gmail.com>
2020-12-03 16:27:42 +0600

    Don't render cost in Node.__repr__() by default

--- pkg/ai/pathfinders/__init__.py
+++ pkg/ai/pathfinders/__init__.py
@@ -9,19 +9,24 @@ class Node:
     _y: int = None
     _cost = 0
     _previous = None
+    _render_cost = False
 
-    def __init__(self, x, y, cost=0):
+    def __init__(self, x, y, cost=0, render_cost=False):
         self._x = x
         self._y = y
         self._id = y * 666 + x
         self._cost = cost
+        self._render_cost = render_cost
 
     def __eq__(self, other):
         return self._id == other.id()
 
     def __str__(self):
-        cost = 'INF' if self._cost == INFINITY else self._cost
-        return f'({self._x}, {self._y}; c={cost})'
+        cost_str = ''
+        if self._render_cost:
+            cost = 'INF' if self._cost == INFINITY else self._cost
+            cost_str = f'; c={cost}'
+        return f'({self._x}, {self._y}{cost_str})'
 
     def __repr__(self):
         return self.__str__()

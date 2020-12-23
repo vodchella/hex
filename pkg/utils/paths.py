aa7a63884ec6e42f2bae225ac3f081c83e14b1c9
vodchella <twister.kz@gmail.com>
2020-12-23 12:12:21 +0600

    Refactor

--- pkg/utils/paths.py
+++ pkg/utils/paths.py
@@ -1,10 +1,10 @@
-import collections
-import itertools
+from collections import Counter
+from itertools import chain
 from pkg.ai.pathfinders import Node
 
 
 def merge_paths(*paths):
-    merged = [p for p in itertools.chain(*paths)]
+    merged = [p for p in chain(*paths)]
     return list(dict.fromkeys(merged))
 
 
@@ -13,4 +13,4 @@ def compare_paths(path1, path2):
         return [Node(p[0], p[1]).id() for p in path]
     ids1 = path_to_ids(path1)
     ids2 = path_to_ids(path2)
-    return collections.Counter(ids1) == collections.Counter(ids2)
+    return Counter(ids1) == Counter(ids2)

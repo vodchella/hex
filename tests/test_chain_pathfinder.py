7ce2acc9c03e371c2efcdc7e479eaac91e1a764b
vodchella <twister.kz@gmail.com>
2020-12-23 17:38:40 +0600

    Add test for ChainPathfinder

--- tests/test_chain_pathfinder.py
+++ tests/test_chain_pathfinder.py
@@ -35,6 +35,7 @@ class TestChainPathfinder:
     def test_1(self):
         pf = ChainPathfinder(_init_test_board())
         path = pf.find_path(PLAYER_ONE, 6, 0, 6, 6)
+
         assert compare_paths(
             path,
             [(6, 0), (3, 3), (4, 2), (5, 1), (3, 4), (6, 6)]
@@ -43,8 +44,10 @@ class TestChainPathfinder:
     def test_2(self):
         board = _init_test_board()
         board.set_cell(1, 2, PLAYER_NONE)
+
         pf = ChainPathfinder(board)
         path = pf.find_path(PLAYER_ONE, 6, 0, 6, 6)
+
         assert compare_paths(
             path,
             [(6, 0), (4, 0), (5, 0), (1, 2), (3, 4), (6, 6)]
@@ -60,3 +63,22 @@ class TestChainPathfinder:
         path = pf.find_path(PLAYER_ONE, 0, 0, 2, 2)
 
         assert len(path) == 0
+
+    def test_4(self):
+        board = Board(7, 7)
+        board.set_cell(5, 1, PLAYER_ONE)
+        board.set_cell(4, 2, PLAYER_ONE)
+        board.set_cell(6, 5, PLAYER_ONE)
+        board.set_cell(5, 5, PLAYER_ONE)
+        board.set_cell(4, 5, PLAYER_ONE)
+        board.set_cell(3, 5, PLAYER_ONE)
+        board.set_cell(2, 5, PLAYER_ONE)
+        board.set_cell(1, 5, PLAYER_ONE)
+
+        pf = ChainPathfinder(board)
+        path = pf.find_path(PLAYER_ONE, 6, 0, 6, 6)
+
+        assert compare_paths(
+            path,
+            [(6, 0), (4, 4), (4, 3), (6, 6)]
+        )
